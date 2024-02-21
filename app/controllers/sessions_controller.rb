f4b9832d6270b393380658049d787575f0ef6624
Mehmet <mehmetkusken@gmail.com>
2024-02-21 15:55:47 +0300

    created token and autologin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,15 @@
 class SessionsController < ApplicationController
 
-    def create
-        user = User.find_by_username(params[:username])
-        if user && user.authenticate(params[:password])
-          render json: {user: UserSerializer.new(user)}
-        else
-          render json: {errors: "Invalid Username and/or Password, get good!"}, status: :forbidden
-        end
+  def create
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      render json: {user: UserSerializer.new(user), token: encode_token(user.id)}
+    else
+      render json: {errors: "Invalid Username and/or Password, get good!"}, status: :forbidden
     end
-    
-      def autologin
-        render json: {user: UserSerializer.new(logged_in_user)}
-      end
+  end
+
+  def autologin
+    render json: {user: UserSerializer.new(logged_in_user), token: encode_token(logged_in_user.id)}
+  end
 end
