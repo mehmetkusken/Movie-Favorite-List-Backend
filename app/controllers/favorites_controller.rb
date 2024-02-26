e80ae0f2d065799c94f4d6293fd895edc5612898
Mehmet <mehmetkusken@gmail.com>
2024-02-26 18:13:54 +0300

    final commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,21 @@
 class FavoritesController < ApplicationController
+
+    def index
+        favorites = logged_in_user.favorites
+        render json: favorites
+    end
+
+    def create
+        movie = Movie.find(params[:movie_id])
+        user = logged_in_user
+        favorite = Favorite.create(movie:movie , user: user )
+        render json: {message: "Successfully added favorite"}
+    end
+
+    def destroy
+    
+        favorite = Favorite.find(params[:id])
+        favorite.destroy
+        render json: {message: "Successfully removed favorite"}
+    end
 end
